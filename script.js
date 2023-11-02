59a95bd6d2a2c8c8b13eff111389287524006217
Liebe974 <137163247+Liebe974@users.noreply.github.com>
2023-11-02 17:12:00 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,138 @@
+const container = document.querySelector(".container");
+const searchBox = document.querySelector(".search-box");
+const inputRange = document.querySelector("#number");
+const inputSpan = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn button");
+let countries = [];
+
+async function fetchCountries() {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    data = await response.json();
+    displayCountries();
+  } catch (error) {
+    console.log(error);
+  }
+}
+    //traduire le nom du pays en français
+
+function displayCountries() {
+  countries = data;
+  countries.forEach((country) => {
+    container.innerHTML += ` 
+    <div class="country">
+    <div class="flag">
+    <img src="${country.flags.png}" alt="${country.name.common}">
+    </div>
+    <div class="country-info">
+    <h2 class="country-name">${country.translations.fra.official}</h2>
+    <h3 class="country-capital">Capital: ${country.capital}</h3>
+    <h3 class="country-population">Population: ${country.population}</h3>
+    </div>
+    `;
+  });
+}
+
+searchBox.addEventListener("input", (e) => {
+  const value = e.target.value;
+  container.innerHTML = "";
+  countries.forEach((country) => {
+    if (country.translations.fra.official.toLowerCase().includes(value.toLowerCase())) {
+      container.innerHTML += ` 
+      <div class="country">
+      <div class="flag">
+      <img src="${country.flags.png}" alt="${country.name.common}">
+      </div>
+      <div class="country-info">
+      <h2 class="country-name">${country.translations.fra.official}</h2>
+      <h3 class="country-population">Population: ${country.population}</h3>
+      <h3 class="country-region">Region: ${country.region}</h3>
+      <h3 class="country-capital">Capital: ${country.capital}</h3>
+      </div>
+      `;
+  }
+});
+})
+
+//Gérer le nombre de pays afficher avec l'input range. Avant le map, vous pouvez utiliser le fonction slice(0, valeurSouhaitée)
+inputRange.addEventListener("input", (e) => {
+  const value = e.target.value;
+  inputSpan.innerHTML = value;
+  container.innerHTML = "";
+  countries.slice(0, value).forEach((country) => {
+    container.innerHTML += ` 
+    <div class="country">
+    <div class="flag">
+    <img src="${country.flags.png}" alt="${country.name.common}">
+    </div>
+    <div class="country-info">
+    <h2 class="country-name">${country.translations.fra.official}</h2>
+    <h3 class="country-population">Population: ${country.population}</h3>
+    <h3 class="country-region">Region: ${country.region}</h3>
+    <h3 class="country-capital">Capital: ${country.capital}</h3>
+    `;
+  });
+})
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerHTML;
+    if (value === "Croissant") {
+      const sortedCountries = countries.sort((a, b) => {
+        return a.population - b.population;
+      });
+      container.innerHTML = "";
+      sortedCountries.forEach((country) => {
+        container.innerHTML += ` 
+        <div class="country">
+        <div class="flag">
+        <img src="${country.flags.png}" alt="${country.name.common}">
+        </div>
+        <div class="country-info">
+        <h2 class="country-name">${country.translations.fra.official}</h2>
+        <h3 class="country-population">Population: ${country.population}</h3>
+        <h3 class="country-region">Region: ${country.region}</h3>
+        <h3 class="country-capital">Capital: ${country.capital}</h3>
+        `;
+      });
+    } else if (value === "Décroissant") {
+      const sortedCountries = countries.sort((a, b) => {
+        return b.population - a.population;
+      });
+      container.innerHTML = "";
+      sortedCountries.forEach((country) => {
+        container.innerHTML += ` 
+        <div class="country">
+        <div class="flag">
+        <img src="${country.flags.png}" alt="${country.name.common}">
+        </div>
+        <div class="country-info">
+        <h2 class="country-name">${country.translations.fra.official}</h2>
+        <h3 class="country-population">Population: ${country.population}</h3>
+        <h3 class="country-region">Region: ${country.region}</h3>
+        <h3 class="country-capital">Capital: ${country.capital}</h3>
+        `;
+      });
+    } else if (value === "Alphabétique") {
+      const sortedCountries = countries.sort((a, b) => {
+        return a.name.common.localeCompare(b.name.common);
+      });
+      container.innerHTML = "";
+      sortedCountries.forEach((country) => {
+        container.innerHTML += ` 
+        <div class="country">
+        <div class="flag">
+        <img src="${country.flags.png}" alt="${country.name.common}">
+        </div>
+        <div class="country-info">
+        <h2 class="country-name">${country.translations.fra.official}</h2>
+        <h3 class="country-population">Population: ${country.population}</h3>
+        <h3 class="country-region">Region: ${country.region}</h3>
+        <h3 class="country-capital">Capital: ${country.capital}</h3>
+        `;
+      });
+    }
+  });
+});
+
+fetchCountries();
